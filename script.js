5dc0f0d35bc0653d572223c6dab75c53ec85464c
DevSushant03 <157278979+DevSushant03@users.noreply.github.com>
2024-04-07 21:06:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+document.querySelector(".DNO-input").oninput = () =>{
+  document.querySelector(".DNO").innerText = document.querySelector(".DNO-input").value;
+}
+
+document.querySelector(".Name-input").oninput = () =>{
+  document.querySelector(".Name").innerText = document.querySelector(".Name-input").value;
+}
+
